e2557b0dd7c65afe5eca9058a7173f0b7cffbb6f
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-12-26 02:41:57 -0500

    Solve the Problem 1 teaser.
    Given the expected sum of 23 with a limit of 10, we can use that
    as a guide toward the solution for a limit of 1000.

--- Problem1.cs
+++ Problem1.cs
@@ -5,5 +5,10 @@ using System.Text;
 
 namespace ProjectEuler {
     public class Problem1 {
+        public int getSumOfMultiplesOf3And5Below (int limit) {
+            return Enumerable.Range(1, limit - 1).Where(
+                x => (x % 3 == 0 || x % 5 == 0)
+            ).Sum();
+        }
     }
 }
--- Tests/Problem1Tests.cs
+++ Tests/Problem1Tests.cs
@@ -14,5 +14,18 @@ namespace Tests {
         public void SetUp () {
             problem = new Problem1();
         }
+
+        [TestCaseSource("GetLimitAndExpectedSumOfMultiplesOf3And5")]
+        public void ShouldGetTheCorrectAnswer (int limit, int expectedSum) {
+            Assert.AreEqual(expectedSum, problem.getSumOfMultiplesOf3And5Below(limit));
+        }
+
+        public object[][] GetLimitAndExpectedSumOfMultiplesOf3And5 () {
+            var data = new object[][] {
+                new object[] { 10, 23 },
+            };
+
+            return data;
+        }
     }
 }
