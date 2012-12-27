ea12846c960e2197addee0cb5392ac5b9087ebef
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-12-26 23:00:52 -0500

    Solve the first part of Problem 3.
    Once I can get the list of prime factors for a number, finding the
    maximum of such a list should be trivial.

---
+++ Problem3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler {
+    public class Problem3 {
+        public List<int> PrimeFactorsOf (int number) {
+            return Enumerable.Range(1, number - 1).Where(
+                x => (x.IsPrime()) && (number % x == 0)
+            ).ToList();
+        }
+    }
+
+    public static class IntExtensions {
+        public static bool IsPrime (this int number) {
+            if ((number % 2) == 0) {
+                return number == 2;
+            }
+
+            int sqrt = (int)Math.Sqrt(number);
+
+            for (int t = 3; t <= sqrt; t = t + 2) {
+                if (number % t == 0) {
+                    return false;
+                }
+            }
+
+            return number != 1;
+        }
+    }
+}
---
+++ Tests/Problem3Tests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using ProjectEuler;
+using System.Collections.Generic;
+
+namespace Tests {
+    [TestFixture]
+    class Problem3Tests {
+        private Problem3 problem;
+
+        [SetUp]
+        public void SetUp () {
+            problem = new Problem3();
+        }
+
+        [Test]
+        public void ShouldReturnCorrectListOfPrimeFactors () {
+            // The prime factors of 13195 are 5, 7, 13 and 29.
+            CollectionAssert.AreEqual(
+                new List<int>() { 5, 7, 13, 29 },
+                problem.PrimeFactorsOf(13195)
+            );
+        }
+    }
+}
