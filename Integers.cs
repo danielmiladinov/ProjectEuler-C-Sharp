dcb0a3818c6c16892f22cf12287a1ece454d5b81
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-12-28 23:41:06 -0500

    Move the Integers extension class to its own file.

---
+++ Integers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectEuler {
+    public static class Integers {
+        public static IEnumerable<int> DescendingRange (int start, int stop) {
+            for (int i = start; i >= stop; i--) {
+                yield return i;
+            }
+        }
+
+        public static bool IsPalindromic (this int candidate) {
+            string forwards = candidate.ToString();
+            char[] digits = forwards.ToCharArray();
+            Array.Reverse(digits);
+            string backwards = new String(digits);
+            return forwards == backwards;
+        }
+    }
+}
--- Problem4.cs
+++ Problem4.cs
@@ -29,20 +29,4 @@ namespace ProjectEuler {
             return palindromes.Max();
         }
     }
-
-    public static class Integers {
-        public static IEnumerable<int> DescendingRange (int start, int stop) {
-            for (int i = start; i >= stop; i--) {
-                yield return i;
-            }
-        }
-
-        public static bool IsPalindromic (this int candidate) {
-            string forwards = candidate.ToString();
-            char[] digits = forwards.ToCharArray();
-            Array.Reverse(digits);
-            string backwards = new String(digits);
-            return forwards == backwards;
-        }
-    }
 }
