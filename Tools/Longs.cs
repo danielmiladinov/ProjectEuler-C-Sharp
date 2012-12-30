356b5e8f3f6f1b8ab528a6dd21a577cc231937cb
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-12-30 18:56:29 -0500

    Add some handy Long extension methods.
    NextPrime() may not be as efficient as it could be, but it'll do for now.

--- Tools/Longs.cs
+++ Tools/Longs.cs
@@ -20,5 +20,35 @@ namespace ProjectEuler.Tools {
         public static bool DivisibleBy (this long dividend, long divisor) {
             return dividend % divisor == 0;
         }
+
+        public static bool IsPrime (this long number) {
+            if ((number % 2) == 0) {
+                return number == 2;
+            }
+            long sqrt = (long)Math.Sqrt(number);
+            for (long t = 3; t <= sqrt; t = t + 2) {
+                if (number % t == 0) {
+                    return false;
+                }
+            }
+            return number != 1;
+        }
+
+        public static long NextPrime (this long number) {
+            switch (number) {
+                case 1: return 2;
+                default: {
+                    long searchLimit = number * 2;
+
+                    for (long i = number + 1; i < searchLimit; ++i) {
+                        if (i.IsPrime()) {
+                            return i;
+                        }
+                    }
+
+                    return searchLimit;
+                }
+            }
+        }
     }
 }
