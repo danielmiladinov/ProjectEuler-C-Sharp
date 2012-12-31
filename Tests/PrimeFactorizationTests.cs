4a83f0bc8032b882c5a7218ec97e1705b7db905c
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-12-30 19:00:26 -0500

    Add a PrimeFactorization class.
    Given any long integer, PrimeFactorization.Of() will return a List
    of PrimeFactors for that number.  A PrimeFactor is a prime number
    with an exponent.

---
+++ Tests/PrimeFactorizationTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ProjectEuler.Tools;
+
+namespace Tests {
+    [TestFixture]
+    class PrimeFactorizationTests {
+
+        [Test]
+        public void OneIsNotPrimeFactorizable () {
+            CollectionAssert.IsEmpty(PrimeFactorization.Of(1));
+        }
+
+        [TestCaseSource("GetSomeNumbersAndTheirPrimeFactorizations")]
+        public void PrimeFactorizationShouldWorkCorrectly (int number, List<PrimeFactor> expectedPrimeFactorization) {
+            CollectionAssert.AreEquivalent(
+                expectedPrimeFactorization,
+                PrimeFactorization.Of(number)
+            );
+        }
+
+        public object[][] GetSomeNumbersAndTheirPrimeFactorizations () {
+            return new object[][] {
+                new object[] { 2, new List<PrimeFactor> { new PrimeFactor(2) }, },
+                new object[] { 3, new List<PrimeFactor> { new PrimeFactor(3) }, },
+                new object[] { 4, new List<PrimeFactor> { new PrimeFactor(2, 2) }, },
+                new object[] { 5, new List<PrimeFactor> { new PrimeFactor(5) }, },
+                new object[] { 6, new List<PrimeFactor> { new PrimeFactor(2), new PrimeFactor(3), }, },
+                new object[] { 7, new List<PrimeFactor> { new PrimeFactor(7), }, },
+                new object[] { 8, new List<PrimeFactor> { new PrimeFactor(2, 3), }, },
+                new object[] { 9, new List<PrimeFactor> { new PrimeFactor(3, 2), }, },
+                new object[] { 10, new List<PrimeFactor> { new PrimeFactor(2), new PrimeFactor(5), }, },
+                new object[] { 11, new List<PrimeFactor> { new PrimeFactor(11), }, },
+                new object[] { 12, new List<PrimeFactor> { new PrimeFactor(2, 2), new PrimeFactor(3), }, },
+                new object[] { 13, new List<PrimeFactor> { new PrimeFactor(13), }, },
+                new object[] { 14, new List<PrimeFactor> { new PrimeFactor(2), new PrimeFactor(7), }, },
+                new object[] { 15, new List<PrimeFactor> { new PrimeFactor(3), new PrimeFactor(5), }, },
+                new object[] { 16, new List<PrimeFactor> { new PrimeFactor(2, 4), }, },
+                new object[] {
+                    232792560,
+                    new List<PrimeFactor> {
+                        new PrimeFactor(2, 4),
+                        new PrimeFactor(3, 2),
+                        new PrimeFactor(5),
+                        new PrimeFactor(7),
+                        new PrimeFactor(11),
+                        new PrimeFactor(13),
+                        new PrimeFactor(17),
+                        new PrimeFactor(19),
+                    },
+                },
+            };
+        }
+    }
+}
---
+++ Tools/PrimeFactorization.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler.Tools {
+    public class PrimeFactorization {
+        public static List<PrimeFactor> Of (long number) {
+            var primeFactors = new List<PrimeFactor>();
+            if (number == 1) {
+                return primeFactors;
+            } else if (number.IsPrime()) {
+                primeFactors.Add(new PrimeFactor(number));
+            } else {
+                long divisor = 2;
+                long exponent = 0;
+
+                while (number >= divisor) {
+                    if (number % divisor == 0) {
+                        exponent += 1;
+                        number = number / divisor;
+                    } else {
+                        if (exponent > 0) {
+                            primeFactors.Add(new PrimeFactor(divisor, exponent));
+                        }
+                        divisor = divisor.NextPrime();
+                        exponent = 0;
+                    }
+                }
+
+                primeFactors.Add(new PrimeFactor(divisor, exponent));
+            }
+
+            return primeFactors;
+        }
+    }
+
+    public struct PrimeFactor {
+        private long baseValue;
+        private long exponentValue;
+
+        public long BaseValue { get { return baseValue; } }
+        public long ExponentValue { get { return exponentValue; } }
+
+        public PrimeFactor (long prime)
+            : this() {
+            baseValue = prime;
+            exponentValue = 1;
+        }
+
+        public PrimeFactor (long prime, long exponent)
+            : this(prime) {
+            exponentValue = exponent;
+        }
+
+        public override string ToString () {
+            return String.Format("{0}^{1}", baseValue, exponentValue);
+        }
+    }
+}
