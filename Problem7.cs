bede0527aeedad007ccc857888f1d22280f90d3d
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-01-01 01:59:11 -0500

    Solve Problem 7.

---
+++ Problem7.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using ProjectEuler.Tools;
+
+namespace ProjectEuler {
+    public class Problem7 {
+        public long FromThePrimeNumberSequenceTheElement (long number) {
+            long theNextPrime = 2;
+            Longs.Range(1, number - 1).ToList().ForEach(x => theNextPrime = theNextPrime.NextPrime());
+            return theNextPrime;
+        }
+    }
+}
---
+++ Tests/Problem7Tests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using ProjectEuler;
+
+namespace Tests {
+    [TestFixture]
+    class Problem7Tests {
+        private Problem7 problem;
+
+        [SetUp]
+        public void SetUp () {
+            problem = new Problem7();
+        }
+
+        [Test]
+        public void ShouldReturnTheCorrectAnswerForTheSixthPrime () {
+            Assert.AreEqual(13, problem.FromThePrimeNumberSequenceTheElement(6));
+        }
+
+        [Test]
+        public void ShouldReturnTheCorrectAnswerForTheTenThousandFirstPrime () {
+            Assert.AreEqual(104743, problem.FromThePrimeNumberSequenceTheElement(10001));
+        }
+    }
+}
