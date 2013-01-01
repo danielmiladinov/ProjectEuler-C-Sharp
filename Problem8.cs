696de0f9778bca7ce883b2ef76693b02ea4113a4
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-01-01 02:43:03 -0500

    Solve Problem 8.

---
+++ Problem8.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace ProjectEuler {
+    public class Problem8 {
+        public int GreatestProductOfFiveConsecutiveDigitsIn (string veryLargeNumber) {
+            return Enumerable.Range(0, veryLargeNumber.Length - 5)
+                .Select(i => veryLargeNumber.Substring(i, 5)
+                    .Select(c => Int32.Parse(c.ToString()))
+                    .Aggregate((x, y) => x * y))
+                    .Max();
+        }
+    }
+}
