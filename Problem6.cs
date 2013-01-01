47d1c909e224ce24651237a0cb76bfbd312c1cf0
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-01-01 01:37:26 -0500

    Solve the teaser for Problem 6.

---
+++ Problem6.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using ProjectEuler.Tools;
+
+namespace ProjectEuler {
+    public class Problem6 {
+        public long SumOfTheSquaresFromOneTo (long number) {
+            return Longs.Range(1, number).Select(x => (long)Math.Pow(x, 2)).Sum();
+        }
+
+        public long SquareOfTheSumsFromOneTo (long number) {
+            return (long)Math.Pow(Longs.Range(1, number).Sum(), 2);
+        }
+    }
+}
---
+++ Tests/Problem6Tests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using ProjectEuler;
+
+namespace Tests {
+    [TestFixture]
+    class Problem6Tests {
+        private Problem6 problem;
+
+        [SetUp]
+        public void SetUp () {
+            problem = new Problem6();
+        }
+
+        [Test]
+        public void SumOfTheSquaresOfTheFirstTenNaturalNumbers () {
+            Assert.AreEqual(385, problem.SumOfTheSquaresFromOneTo(10));
+        }
+
+        [Test]
+        public void SquareOfTheSumOfTheFirstTenNaturalNumbers () {
+            Assert.AreEqual(3025, problem.SquareOfTheSumsFromOneTo(10));
+        }
+
+        [Test]
+        public void DifferenceOfTheSquareOfTheSumsAndTheSumOfTheSquaresOfTheFirstTenNaturalNumbers () {
+            Assert.AreEqual(2640, problem.SquareOfTheSumsFromOneTo(10) - problem.SumOfTheSquaresFromOneTo(10));
+        }
+    }
+}
