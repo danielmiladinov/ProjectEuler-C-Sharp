648559724ffb05025758902bd17db3c2bb489183
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-01-13 03:49:08 -0500

    Solve Problem 10.
    Build on tools I previously developed to implement a generator
    to produce a sequence of primes and take its sum.
    
    It runs a little long, but that's a simple matter of improving the
    run speed of Longs.NextPrime().

---
+++ Problem10.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProjectEuler.Tools;
+
+namespace ProjectEuler {
+    public class Problem10 {
+        public long getSummationOfPrimesBelow (long upperLimit) {
+            return GetPrimesBelow(upperLimit).Sum();
+        }
+
+        public IEnumerable<long> GetPrimesBelow (long upperLimit) {
+            for (long i = 2L; i < upperLimit; i = i.NextPrime()) {
+                yield return i;
+            }
+        }
+    }
+}
---
+++ Tests/Problem10Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProjectEuler;
+using NUnit.Framework;
+
+namespace Tests {
+    [TestFixture]
+    class Problem10Tests {
+        private Problem10 problem;
+
+        [SetUp]
+        public void SetUp () {
+            problem = new Problem10();
+        }
+
+        [TestCaseSource("getSomeUpperLimitsAndTheExpectedSumOfAllPrimesBelowThem")]
+        public void ShouldReturnTheCorrectSummationOfPrimes (long upperLimit, long expectedSummationOfPrimes) {
+            Assert.AreEqual(expectedSummationOfPrimes, problem.getSummationOfPrimesBelow(upperLimit));
+        }
+
+
+        public object[][] getSomeUpperLimitsAndTheExpectedSumOfAllPrimesBelowThem () {
+            return new object[][] {
+                new object[] { 10L, 17L },
+                new object[] { 2000000L, 142913828922L }
+            };
+        }
+    }
+}
