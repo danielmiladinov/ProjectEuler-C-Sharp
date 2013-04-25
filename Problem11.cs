c3751d7f5781a734d85dc710ce2b8f8c8fb0511e
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-04-25 02:24:19 -0400

    Solve Problem 11.
    getLargestProductIn(grid) isn't that bad for a first pass at the solution.
    But I'm not too keen on how I had to manually project the 4x4 sub-grids.
    
    Since I'm taking subgrids starting from the top-left corner, I can count
    from 0 to 16, where 16 is 4 less than the length of sides of the grid.
    
    I'm really impressed with the extension methods that LINQ provides,
    even if some of them are named funny:
    
    * Select() instead of map,
    * Aggregate() instead of reduce,
    * SelectMany() instead of flatten?
    
    But hey, a rose by any other name... still smells pretty sweet!

---
+++ Problem11.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler {
+    public class Problem11 {
+        public int getLargestProductIn (int[][] grid) {
+            return Enumerable.Range(0, 16).Select(r =>
+                Enumerable.Range(0, 16).Select(c => {
+                    var grid4x4 = new int[][] {
+                        new int [] { grid[r + 0][c + 0], grid[r + 0][c + 1], grid[r + 0][c + 2], grid[r + 0][c + 3], },
+                        new int [] { grid[r + 1][c + 0], grid[r + 1][c + 1], grid[r + 1][c + 2], grid[r + 1][c + 3], },
+                        new int [] { grid[r + 2][c + 0], grid[r + 2][c + 1], grid[r + 2][c + 2], grid[r + 2][c + 3], },
+                        new int [] { grid[r + 3][c + 0], grid[r + 3][c + 1], grid[r + 3][c + 2], grid[r + 3][c + 3], },
+                    };
+
+                    var products = new List<int>();
+
+                    // 4 rows across
+                    products.AddRange(
+                        Enumerable.Range(0, 4).Select(row =>
+                            grid4x4[row].Aggregate((x, y) => x * y)
+                        )
+                    );
+
+                    // 4 rows down
+                    products.AddRange(
+                        Enumerable.Range(0, 4).Select(col =>
+                            Enumerable.Range(0, 4).Select(row =>
+                                grid4x4[row][col]
+                            ).Aggregate((x, y) => x * y)
+                        )
+                    );
+
+                    // top-left to bottom-right diagonal
+                    products.Add(
+                        Enumerable.Range(0, 4).Select(rowCol =>
+                            grid4x4[rowCol][rowCol]
+                        ).Aggregate((x, y) => x * y)
+                    );
+
+                    // bottom-left to top-right diagonal
+                    products.Add(
+                        Enumerable.Range(0, 4).Reverse().Select(row => {
+                            // col is always the absolute value of row minus 3, 
+                            // it increases as row decreases
+                            return grid4x4[row][Math.Abs(row - 3)];
+                        }).Aggregate((x, y) => x * y)
+                    );
+
+                    return products.Max();
+                })
+            ).SelectMany(x => x).Max();
+        }
+    }
+}
