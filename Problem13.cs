62859d9629c360ade18f26f7c7f22cc80ec08129
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-05-06 01:26:40 -0400

    Solve Problem13.
    I suppose this would have been much harder/
    more interesting were not BigInteger available to me.

---
+++ Problem13.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace ProjectEuler {
+    public class Problem13 {
+        public string getFirstTenDigitsFromTheSumOf (List<string> bigNumbers) {
+            return bigNumbers
+                .Select(str => BigInteger.Parse(str))
+                .Aggregate((x, y) => x + y)
+                .ToString()
+                .Substring(0, 10);
+        }
+    }
+}
