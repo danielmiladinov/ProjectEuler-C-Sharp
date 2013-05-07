766ab202a75f963bcfbc96485721dfe317de5ccc
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-05-06 22:46:49 -0400

    Develop a generator of the Collatz Sequence for Problem 14.

---
+++ Problem14.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProjectEuler.Tools;
+
+namespace ProjectEuler {
+    public class Problem14 {
+        public IEnumerable<long> CollatzSequenceOf (long start) {
+            var i = start;
+
+            while (i > 1) {
+                yield return i;
+
+                if (i.DivisibleBy(2)) {
+                    i /= 2;
+                } else {
+                    i = 3 * i + 1;
+                }
+            }
+
+            yield return 1;
+        }
+    }
+}
---
+++ Tests/Problem14Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProjectEuler;
+using NUnit.Framework;
+
+namespace Tests {
+    [TestFixture]
+    class Problem14Tests {
+        private Problem14 problem;
+
+        [SetUp]
+        public void SetUp () {
+            problem = new Problem14();
+        }
+
+        [Test]
+        public void ShouldProduceTheCorrectCollatzSequenceOfThirteen () {
+            CollectionAssert.AreEqual(
+                new List<int> { 13, 40, 20, 10, 5, 16, 8, 4, 2, 1 },
+                problem.CollatzSequenceOf(13).TakeWhile(i => i >= 1).ToList()
+            );
+        }
+    }
+}
